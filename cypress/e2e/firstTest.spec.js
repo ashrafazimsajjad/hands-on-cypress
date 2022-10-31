6b7e765fa6aa5f1064f06ff7cce351dce0b9a998
ashrafazimsajjad <ashrafazimsajjad@gmail.com>
2022-10-31 18:49:59 +0600

    improving

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -39,4 +39,27 @@ describe('Our first suite', () => {
         //Most recommended way by cypress
         cy.get('[data-cy="imputEmail1"]') 
     })
+
+    it.only('Second test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain','Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+    })
 })
