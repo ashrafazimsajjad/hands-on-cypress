e089eeef67a4a66930520b92f746cc13a67bdb04
ashrafazimsajjad <ashrafazimsajjad@gmail.com>
2022-11-14 18:43:15 +0600

    Invoke command added

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -63,7 +63,7 @@ describe('Our first suite', () => {
         cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
     })
 
-    it.only('Then and wrap methods', () => {
+    it('Then and wrap methods', () => {
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -97,5 +97,35 @@ describe('Our first suite', () => {
             })
 
         })
+    })
+
+    it.only('Invoke Command', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain','Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then( label => {
+            expect(label.text()).to.equal('Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card','Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr','class')
+            //.should('contain','checked')
+            .then(classValue => {
+                expect(classValue).to.contain('checked')
+            })
     })
 })
